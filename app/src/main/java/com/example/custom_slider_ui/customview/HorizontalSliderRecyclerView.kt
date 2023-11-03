f4740bbda065a36f6318849c3bba57cd963c6e56
h1eu <letrunghieu27102000@gmail.com>
2023-11-03 17:53:53 +0700

    add

---
+++ app/src/main/java/com/example/custom_slider_ui/customview/HorizontalSliderRecyclerView.kt
@@ -0,0 +1,12 @@
+package com.example.custom_slider_ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class HorizontalSliderRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+): RecyclerView(context,attributeSet) {
+
+}
